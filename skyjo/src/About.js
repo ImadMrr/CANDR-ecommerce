a398d523d9e82b8007f631ae91bd33f5f23338ec
Youcefffj <youcef.yaich.pro@outlook.fr>
2024-05-08 12:59:07 +0900

    about moche

--- skyjo/src/About.js
+++ skyjo/src/About.js
@@ -1,14 +1,17 @@
-// Home.js, Products.js, About.js, Contact.js
+// About.js
 
-import React from 'react';
+import React from "react";
+import './styles/About.css'; // Importer le fichier CSS pour appliquer les styles
 
 function About() {
   return (
-    <div>
+    <div className="about-container">
       <h2>About</h2>
-      {/* Ajoutez ici le contenu de la page d'accueil */}
+      <p>Welcome to our About page! Skyjo is a leading provider of high-quality products for your everyday needs. Our mission is to deliver innovative solutions that enhance your lifestyle.</p>
+      <p>Founded in 20XX, Skyjo has quickly become a trusted name in the industry. With a focus on customer satisfaction and product excellence, we strive to exceed expectations with every interaction.</p>
+      <p>Thank you for choosing Skyjo. We look forward to serving you!</p>
     </div>
   );
 }
 
-export default About; // Assurez-vous d'exporter le composant Home
+export default About;
