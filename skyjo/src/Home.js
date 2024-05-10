777a29fa7cacbc705c49bfbdd5bebc2319925f4c
Youcefffj <youcef.yaich.pro@outlook.fr>
2024-05-10 14:57:20 +0900

    home temp

--- skyjo/src/Home.js
+++ skyjo/src/Home.js
@@ -4,9 +4,28 @@ function Home() {
   return (
     <div>
       <h2>Home</h2>
-      {/* Ajoutez ici le contenu de la page d'accueil */}
+      
+      {/* Fenêtre de recommandations */}
+      <div className="recommendations">
+        <h3>Recommandations</h3>
+        <ul>
+          <li>Produit recommandé 1</li>
+          <li>Produit recommandé 2</li>
+          <li>Produit recommandé 3</li>
+        </ul>
+      </div>
+
+      {/* Fenêtre de réductions */}
+      <div className="discounts">
+        <h3>Réductions</h3>
+        <ul>
+          <li>Produit en promotion 1</li>
+          <li>Produit en promotion 2</li>
+          <li>Produit en promotion 3</li>
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default Home; // Assurez-vous d'exporter le composant Home
+export default Home;
