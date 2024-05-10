31f3fa47971cd1b2acc4bcf93e15c9e606d8e1b8
Youcefffj <youcef.yaich.pro@outlook.fr>
2024-05-10 15:16:28 +0900

    HOME

--- skyjo/src/Home.js
+++ skyjo/src/Home.js
@@ -5,23 +5,16 @@ function Home() {
     <div>
       <h2>Home</h2>
       
-      {/* Fenêtre de recommandations */}
       <div className="recommendations">
         <h3>Recommandations</h3>
         <ul>
-          <li>Produit recommandé 1</li>
-          <li>Produit recommandé 2</li>
-          <li>Produit recommandé 3</li>
+          <li>AJOUT DYNAMIC</li>
         </ul>
       </div>
-
-      {/* Fenêtre de réductions */}
       <div className="discounts">
         <h3>Réductions</h3>
         <ul>
-          <li>Produit en promotion 1</li>
-          <li>Produit en promotion 2</li>
-          <li>Produit en promotion 3</li>
+          <li>AJOUT DYNAMIC</li>
         </ul>
       </div>
     </div>
